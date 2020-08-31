08b42d2d22697be9570fed4ded5302f05287bd66
saurabh5788 <saurabh.singh5788@gmail.com>
2020-08-31 10:55:04 +0530

    Initial Commit

---
+++ src/test/java/com/example/bank/bankcustomerapp/BankCustomerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank.bankcustomerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCustomerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
